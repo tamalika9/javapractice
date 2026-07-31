6797e6cf55241073f0350593608981d3fdd56cb6
Tamalika Srimani <tamalikasrimani@gmail.com>
2026-07-31 13:42:19 +0530

    GitHub - First Java Program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.println("My name is Tamalika Srimani");
+        System.out.println("I am learning Java");
+        System.out.println("I will become a software developer");
+    }
+}

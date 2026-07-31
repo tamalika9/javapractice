0794f8b8937e5bdb00dd9fd4e9a825b26366834a
Tamalika Srimani <tamalikasrimani@gmail.com>
2026-07-31 23:54:12 +0530

    Added Factorial Java program

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String args[]) {
+        Scanner ob  = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = ob.nextInt();
+        int fact= 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact* i;
+        }
+        System.out.println("Factorial of " + n + " is: " + fact);
+    }
+}
